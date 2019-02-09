0b67e7479202b03279535185312d209e5d836b83
ManyamMonika <47392185+ManyamMonika@users.noreply.github.com>
2019-02-09 05:43:38 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,19 @@
-var cache="c1";
-this.addEventListener("install",function(event){
-	event.waitUntil(
-		caches.open(cache1).then(cache=>{
-			cache.addAll([
-				])
-		})
-		);
+var cache1 = "c1"
+this.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(cache1).then(cache => {
+            cache.addAll([])
+        })
+    );
 });
-this.addEventListener('fetch',function(event){
-	event.respondWith(
-		caches.open(cache1).then(function(cache){
-		return cache.match(event.request).then(function(res){
-			return res || fetch(event.request).then(function(reqres){
-				cache.put(event.request,reqres.clone());
-			})
-		})
-	})
-	)
+this.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(cache1).then(function (cache) {
+            return cache.match(event.request).then(function (res) {
+                return res || fetch(event.request).then(function (reqres) {
+                    cache.put(event.request, reqres.clone());
+                })
+            })
+        })
+    )
 })
-
